5dc101cab320740a53a4c76044b0a73e0f705243
alex <18756003663@139.com>
2016-10-12 16:53:20 +0800

    e

--- src/dong/HelloGitHub.java
+++ src/dong/HelloGitHub.java
@@ -7,8 +7,13 @@ package dong;
  */
 public class HelloGitHub {
 	public static void main(String[] args) {
+		final int S = 21;
 		System.out.println("This is commit to GitHub");
 		int i = 0b11111;//JDK1.7以后可以使用
 		System.out.println(i);
+		Double f = 3.14;
+		System.out.println(Double.NaN);
+		char c = '\u005e';
+		System.out.println(Character.isJavaIdentifierStart('$'));
 	}
 }
