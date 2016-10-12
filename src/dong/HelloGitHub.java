5dec6fd77af09a5ac09654c5d3438d838f4f77f4
alex <18756003663@139.com>
2016-10-12 15:56:53 +0800

    注释

--- src/dong/HelloGitHub.java
+++ src/dong/HelloGitHub.java
@@ -8,7 +8,7 @@ package dong;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("This is commit to GitHub");
-		int i = 0b11111;
+		int i = 0b11111;//JDK1.7以后可以使用
 		System.out.println(i);
 	}
 }
