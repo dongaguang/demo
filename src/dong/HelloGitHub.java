74561c94669f9595f31466a8805553a47834510b
alex <18756003663@139.com>
2016-10-12 16:01:50 +0800

    多少

--- src/dong/HelloGitHub.java
+++ src/dong/HelloGitHub.java
@@ -9,6 +9,6 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("This is commit to GitHub");//
 		int i = 0b11111;//JDK1.7以后可以使用
-		System.out.println(i);
+		System.out.println(i);//
 	}
 }
