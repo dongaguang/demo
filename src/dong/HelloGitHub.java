e5e3d17de398903904c2bf3338174898968b27bf
alex <18756003663@139.com>
2016-10-12 15:00:08 +0800

    111

--- src/dong/HelloGitHub.java
+++ src/dong/HelloGitHub.java
@@ -8,5 +8,7 @@ package dong;
 public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("This is commit to GitHub");
+		int i = 0b11111;
+		System.out.println(i);
 	}
 }
