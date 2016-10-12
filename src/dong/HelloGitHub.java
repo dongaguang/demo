efe7c96b4d4c03017348f6140e741ccbba1fb752
dongaguang <18756003663@139.com>
2016-10-12 14:59:51 +0800

    12

--- src/dong/HelloGitHub.java
+++ src/dong/HelloGitHub.java
@@ -7,6 +7,6 @@ package dong;
  */
 public class HelloGitHub {
 	public static void main(String[] args) {
-		System.out.println("This is commit to GitHub");
+		System.out.println("This is commit to GitHub");dddd
 	}
 }
