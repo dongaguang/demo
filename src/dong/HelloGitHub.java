b05ecc22e5cda513ca5911f95f58783f34446928
alex <18756003663@139.com>
2016-10-12 18:51:04 +0800

    enum

--- src/dong/HelloGitHub.java
+++ src/dong/HelloGitHub.java
@@ -6,14 +6,16 @@ package dong;
  *
  */
 public class HelloGitHub {
+	enum PokerColor{A,B}
 	public static void main(String[] args) {
 		final int S = 21;
 		System.out.println("This is commit to GitHub");
 		int i = 0b11111;//JDK1.7以后可以使用
 		System.out.println(i);
-		Double f = 3.14;
+		Double f = 4d;
 		System.out.println(Double.NaN);
 		char c = '\u005e';
-		System.out.println(Character.isJavaIdentifierStart('$'));
+		System.out.println(Math.sqrt(f));
+		PokerColor pc = PokerColor.A;
 	}
 }
