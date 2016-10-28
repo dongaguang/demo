c2ad1edac8025f210d651157943cc0c85de12b40
alex <18756003663@139.com>
2016-10-28 16:10:26 +0800

    1111111

---
+++ src/io/TestIO.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class TestIO {
+	public static void main(String[] args) throws Exception {
+//		//打印系统当前的工作路径
+//		Scanner sc = new Scanner(new FileInputStream("nihao.txt"),"GBK");
+//		while(sc.hasNext()){
+//			System.out.println(sc.nextLine());
+//		}
+	}
+}
