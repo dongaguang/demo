03a887f3dc7fecfdeeffabcea6c502513eb377b0
alex <18756003663@139.com>
2016-12-20 17:33:11 +0800

    111

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Test {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		System.out.println(null == list);
+		System.out.println(list.isEmpty());
+		System.out.println(list.size());
+	}
+}
